4b3fd872b0016a356cfe04abeedaf5c1d3fa1766
GregBates <gdbates1417@gmail.com>
2020-04-13 13:50:16 -0500

    cleaned up previous commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ButtonColor } from './shared/button-colors.enum';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simon';
+
+  //Button Colors
+  redColor: ButtonColor = ButtonColor.red;
+  blueColor: ButtonColor = ButtonColor.blue;
+  greenColor: ButtonColor = ButtonColor.green;
+  yellowColor: ButtonColor = ButtonColor.yellow;
 }
--- src/app/components/color-button/color-button.component.ts
+++ src/app/components/color-button/color-button.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ButtonColor } from 'src/app/shared/button-colors.enum';
 
 @Component({
   selector: 'app-color-button',
@@ -12,7 +13,7 @@ export class ColorButtonComponent implements OnInit {
  * 1. should accept a color as an input
  * 2. color should be an enum
  * 3. should have an eventEmitter/output pair to say it's been clicked
- * 4. should be a <button>
+ * x. should be a <button>
  * 5. should be a big square the color of it's color input
  * 6. should flash a brighter shade of its color when clicked
  * 7. should make a noise when clicked
@@ -20,9 +21,15 @@ export class ColorButtonComponent implements OnInit {
  * 9. Needs to have a way to know via user input (click) to flash OR via app input (sequence) to flash
  */
 
+  @Input() color:ButtonColor;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  buttonClicked(color:ButtonColor):void {
+    alert(`you pushed the ${color} button!`);
   }
 
 }
