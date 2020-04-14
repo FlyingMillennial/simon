79ca8b6564c3f9402cf0567b10d113f962c223ab
GregBates <gdbates1417@gmail.com>
2020-04-14 09:26:21 -0500

    working game via console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ButtonColor } from './shared/button-colors.enum';
 
 /**
  * TODO:
- * 1. Hold on to a sequence of colors
- * 2. Add a random color
+ * x. Hold on to a sequence of colors
+ * x. Add a random color
  * 3. clear out the colors
  * 4. hold on to an number of inputs received
  * 5. use a coloroutput from a button in concert with the inputsReceived value to check validity of an input
@@ -19,17 +19,68 @@ import { ButtonColor } from './shared/button-colors.enum';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title:string = 'simon';
-  failed:boolean = false;
+export class AppComponent implements OnInit {
+  public title:string = 'simon';
+  public failed:boolean = false;
+  private sequence:ButtonColor[] = [];
+  private currentInputIndex:number = 0;
 
-  redColor: ButtonColor = ButtonColor.red;
-  blueColor: ButtonColor = ButtonColor.blue;
-  greenColor: ButtonColor = ButtonColor.green;
-  yellowColor: ButtonColor = ButtonColor.yellow;
+  public redColor: ButtonColor = ButtonColor.red;
+  public blueColor: ButtonColor = ButtonColor.blue;
+  public greenColor: ButtonColor = ButtonColor.green;
+  public yellowColor: ButtonColor = ButtonColor.yellow;
 
-  handleButtonClick(color:ButtonColor):void {
-    console.log(`Clicked the ${color} button!  Handled by AppComponent`);
+  ngOnInit():void {
+    this.startGame();
+  }
+
+  private startGame():void {
+    this.failed = false;
+    this.sequence = this.addSequenceItem(this.sequence);
+    console.log(this.sequence);
+  }
+
+  public handleButtonClick(color:ButtonColor):void {
+    const validInput = this.isUserInputValid(this.currentInputIndex, this.sequence, color);
+    if (validInput) {
+      this.currentInputIndex++;
+      if (this.currentInputIndex === this.sequence.length) {
+        this.currentInputIndex = 0;
+        this.sequence = this.addSequenceItem(this.sequence);
+        console.log(this.sequence);
+      }
+    } else {
+      this.sequence = [];
+      this.failed = true;
+      setTimeout(() => {
+        this.startGame();
+      }, 2000);
+    }
+  }
+
+  private addSequenceItem(sequence:ButtonColor[]):ButtonColor[] {
+    const myNumber = Math.floor(Math.random() * Math.floor(4));
+    let newSequence = sequence.slice();
+    switch (myNumber) {
+      case 0:
+        newSequence.push(ButtonColor.red);
+        break;
+      case 1:
+        newSequence.push(ButtonColor.blue);
+        break;
+      case 2:
+        newSequence.push(ButtonColor.yellow);
+        break;
+      case 3:
+        newSequence.push(ButtonColor.green);
+        break;
+    }
+    return newSequence;
+  }
+
+  private isUserInputValid(index:number, sequence:ButtonColor[], userInput:ButtonColor):boolean {
+    const isValid = userInput === sequence[index];
+    return isValid;
   }
 
 }
